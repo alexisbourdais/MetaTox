8fde4f774d55fd5c3d461c77b59cda0da85d0b9d
alexisbourdais <108393526+alexisbourdais@users.noreply.github.com>
2024-11-03 20:53:46 +0100

    Add files via upload

--- MetaTox.sh
+++ MetaTox.sh
@@ -68,6 +68,8 @@ Nicotine,CN1CCC[C@H]1c2cccnc2
 
     OPTIONAL parameter
 
+        -m|--metapred   To activate metapredictor
+
         -t|--type   Type of biotransformation to use with Biotransformer3:
                        [allHuman]  : Predicts all possible metabolites from any applicable reaction(Oxidation, reduction, (de-)conjugation) at each step 
                         ecbased    : Prediction of promiscuous metabolism (e.g. glycerolipid metabolism). EC-based metabolism is also called Enzyme Commission based metabolism
@@ -123,6 +125,10 @@ while [ $# -gt 0 ] ; do
             shift
             shift
             ;;
+        -m|--metapred)
+            metapred_activate=true
+            shift
+            ;;
         *)
             help_msg
             exit 0                         
@@ -143,31 +149,6 @@ DirBiotrans="${work_dir}/biotransformer3.0jar/"
 DirMetatrans="${work_dir}/MetaTrans/"
 DirMetapred="${work_dir}/Meta-Predictor/"
 DirCondaEnv="${work_dir}/CondaEnv/"
-
-#########################
-### Environment conda ###
-#########################
-
-eval "$(conda shell.bash hook)"
-#conda init --all
-
-if conda info --envs | grep -q metatrans; then 
-    echo "Conda environment "metatrans" already exists"
-else 
-    conda env create --name metatrans --file ${DirCondaEnv}metatrans_environment.yml
-fi
-
-if conda info --envs | grep -q rdkit; then 
-    echo "Conda environment "rdkit" already exists"
-else 
-    conda env create --name rdkit --file ${DirCondaEnv}rdkit_environment.yml
-fi
-
-if conda info --envs | grep -q metapredictor; then 
-    echo "Conda environment "metapredictor" already exists"
-else 
-    conda env create --name metapredictor --file ${DirCondaEnv}metapred_environment.yml
-fi
 
 ##############
 ### Zenity ###
@@ -201,42 +182,37 @@ if [ -z $input ]; then
     option_biotrans=$(zenity --forms --title="Biotransformer options" --text="Directly Validate to apply default values" --add-entry="The number of steps for the prediction [default=2]" --add-entry="CYP450 prediction Mode: 1=CypReact+BioTransformer rules; 2=CyProduct only; 3=CypReact+BioTransformer rules+CyProducts [Default=3]" --separator=",")
         nstep=$(echo "$option_biotrans" | cut -d "," -f1)
         cmode=$(echo "$option_biotrans" | cut -d "," -f2)
+
+    zenity --question --title="Meta-Predictor Activation" --text="Do you want to activate Meta-Predictor ?"
+    metapred_activate_answer=$?
+    if [ $metapred_activate_answer -eq 0 ]; then
+	    metapred_activate=true
+    else [ $metapred_activate_answer -eq 1 ]
+    	metapred_activate=false
+    fi
 fi
 
 ###############
-###  Input  ###
+###  Option ###
 ###############
-
-file $input | grep CRLF && sudo dos2unix $input
-
-declare -a tab_molecule
-declare -a tab_smiles
-
-while read a
-do
-    Molecule=$(echo $a | cut -d\, -f1)
-    Smiles=$(echo $a | cut -d\, -f2)
-    tab_smiles[${#tab_smiles[@]}]=${Smiles}
-    tab_molecule[${#tab_molecule[@]}]=${Molecule}
-done < $input
-
-path_input="$(realpath ${input})"
 
 ##### Bio-Transformer option
 ###Default Mode
 if [ -z $type ]; then
 	type="allHuman"
 fi
+
 ###Default nsteps
 if [ -z $nstep ] || [[ ! $nstep = +([0-9]) ]]; then
-	nstep="2"
+	nstep="1"
 fi
+
 ###Default cmode
 if [ -z $cmode ] || [[ ! $cmode = +([0-3]) ]]; then
 	cmode="3"
 fi
 
-#####Meta-Trans option
+##### Meta-Trans option
 ###Beam par défaut
 if [ -z $BEAM ] || [[ ! $BEAM = +([0-9]) ]]; then
 	BEAM=5
@@ -252,7 +228,7 @@ if [ -z $MAX ] || [[ ! $MAX = +([0-9]) ]]; then
 	MAX=120
 fi
 
-#####Sygma option
+##### Sygma option
 ###phase1 par défaut
 if [ -z $phase1 ] || [[ ! $phase1 = +([0-9]) ]]; then
 	phase1=1
@@ -262,6 +238,58 @@ fi
 if [ -z $phase2 ] || [[ ! $phase2 = +([0-9]) ]]; then
 	phase2=1 
 fi
+
+##### Meta-Predictor option
+###Activation
+if [ -z $metapred_activate ]; then
+	metapred_activate=false
+fi
+
+#########################
+### Environment conda ###
+#########################
+
+eval "$(conda shell.bash hook)"
+#conda init --all
+
+if conda info --envs | grep -q metatrans; then 
+    echo "Conda environment "metatrans" already exists"
+else 
+    conda env create --name metatrans --file ${DirCondaEnv}metatrans_environment.yml
+fi
+
+if conda info --envs | grep -q rdkit; then 
+    echo "Conda environment "rdkit" already exists"
+else 
+    conda env create --name rdkit --file ${DirCondaEnv}rdkit_environment.yml
+fi
+
+if $metapred_activate; then
+    if conda info --envs | grep -q metapredictor; then 
+        echo "Conda environment "metapredictor" already exists"
+    else 
+        conda env create --name metapredictor --file ${DirCondaEnv}metapred_environment.yml
+    fi
+fi
+
+###############
+###  Input  ###
+###############
+
+file $input | grep CRLF && dos2unix $input
+
+declare -a tab_molecule
+declare -a tab_smiles
+
+while read a
+do
+    Molecule=$(echo $a | cut -d\, -f1)
+    Smiles=$(echo $a | cut -d\, -f2)
+    tab_smiles[${#tab_smiles[@]}]=${Smiles}
+    tab_molecule[${#tab_molecule[@]}]=${Molecule}
+done < $input
+
+path_input="$(realpath ${input})"
 
 ###############
 ### Program ###
@@ -274,10 +302,14 @@ do
 done
 
 metatrans_function --script ${DirScripts} --input "${path_input}" --outdir ${DirOutput} --metadir ${DirMetatrans} --min ${MIN} --max ${MAX} --beam ${BEAM}
-metapredictor_function --script ${DirScripts} --input "${path_input}" --outdir ${DirOutput} --metadir ${DirMetapred}
+
+if $metapred_activate; then
+    metapredictor_function --script ${DirScripts} --input "${path_input}" --outdir ${DirOutput} --metadir ${DirMetapred}
+fi
     
     echo "
     Recording results in : ${DirOutput}
 
     Execution completed !
     "
+
